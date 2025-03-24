accd1a437a78afef14166fab249dafb9a2de0621
yiyi <yl13658394812@gmail.com>
2025-03-24 16:37:08 +0800

    更新stock:修改测试打印机脚本

--- 测试打印机.py
+++ 测试打印机.py
@@ -1,9 +1,12 @@
+import time
+
 from dtpweb import DTPWeb
 
-def print_text(stock_name,barcode):
+def print_text(stock_name,barcode,number):
     '''
     stock_name:  物品名称
     barcode: 商品条码
+    number: 打印数量
     '''
     ##初始化打印机
     api = DTPWeb()
@@ -18,18 +21,20 @@ def print_text(stock_name,barcode):
     text_height = 4     ##条形码下面的数字高度
     # 打开打印机
     if api.open_printer(**printers[0]):
-        # 创建打印任务，指定标签纸大小。
-        api.start_job(label_width, label_height)
+        for i in range(number):
+            # 创建打印任务，指定标签纸大小。
+            api.start_job(label_width, label_height)
 
-        # 绘制字符串
-        api.draw_text(stock_name,4,3)       ##物品名称   绘制字符串在标签纸上的x y的位置
-        # 绘制一维码，一维码下面的字符串拉伸显示
-        api.draw_barcode(barcode, 0, 10, 50,20, text_height)    ##条形码数据   绘制条形码在标签纸上的x y的位置   条形码的高度  条形码下面数字的高度
-        # 提交打印任务，开始打印
-        api.commit_job()
+            # 绘制字符串
+            api.draw_text(stock_name,4,3)       ##物品名称   绘制字符串在标签纸上的x y的位置
+            # 绘制一维码，一维码下面的字符串拉伸显示
+            api.draw_barcode(barcode, 0, 10, 50,20, text_height)    ##条形码数据   绘制条形码在标签纸上的x y的位置   条形码的高度  条形码下面数字的高度
+            # 提交打印任务，开始打印
+            api.commit_job()
+            time.sleep(2)
     else:
         print("打印机链接失败！")
         return None
     # 关闭打印机
     api.close_printer()
-print_text('物品BBBBB物品','1234567890113')
+print_text('物品BBBBB物品','1234567890113',20)
